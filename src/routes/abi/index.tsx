c0c6135800d64d0a865f3e073fc9f021233c297e
Hariprasath137 <hariprasath@yuvabe.com>
2026-01-22 10:09:40 +0530

    fix:removed changes

--- src/routes/abi/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/abi/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/abi/"!</div>
-}
