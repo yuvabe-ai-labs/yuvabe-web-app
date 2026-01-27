3b4cd395a345fe85fe394071cb4bc98ee74d1042
Hariprasath137 <hariprasath@yuvabe.com>
2026-01-27 15:04:21 +0530

    fix:updated notification indicator

--- src/App.tsx
+++ src/App.tsx
@@ -25,12 +25,12 @@ export function App() {
 
   return (
     <>
-      <div className="flex flex-col h-screen w-full overflow-hidden">
+      <div className="flex flex-col  h-dvh w-full overflow-hidden ">
         <div className="z-50 shrink-0">
           <NotificationIndicator />
         </div>
 
-        <div className="flex-1 relative w-full h-full overflow-hidden">
+        <div className="flex-1 relative w-full  overflow-hidden flex flex-col">
           <Toaster position="top-center" richColors />
           <RouterProvider router={router} />
         </div>
