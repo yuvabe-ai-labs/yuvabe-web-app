01c4d0ea57ac83e975b5ab81be9b14eb5e87c01d
Hariprasath137 <hariprasath@yuvabe.com>
2026-01-28 10:48:25 +0530

    fix:updated protected route

--- src/routes/payslip/index.tsx
+++ src/routes/payslip/index.tsx
@@ -1,6 +1,8 @@
+import { requireAuth } from "@/hooks/useRouteGuards";
 import { createFileRoute } from "@tanstack/react-router";
 import PayslipScreen from "./-components/PayslipScreen";
 
 export const Route = createFileRoute("/payslip/")({
   component: PayslipScreen,
+  beforeLoad: requireAuth,
 });
