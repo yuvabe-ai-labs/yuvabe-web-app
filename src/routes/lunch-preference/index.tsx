3306047986bc0439c076b50a19d13aaf37952eaf
Hariprasath137 <hariprasath@yuvabe.com>
2026-01-28 10:50:34 +0530

    fix:updated protected route

--- src/routes/lunch-preference/index.tsx
+++ src/routes/lunch-preference/index.tsx
@@ -1,6 +1,8 @@
+import { requireAuth } from "@/hooks/useRouteGuards";
 import { createFileRoute } from "@tanstack/react-router";
 import LunchPreferenceScreen from "./-component/LunchPreferenceScreen";
 
 export const Route = createFileRoute("/lunch-preference/")({
   component: LunchPreferenceScreen,
+  beforeLoad: requireAuth,
 });
